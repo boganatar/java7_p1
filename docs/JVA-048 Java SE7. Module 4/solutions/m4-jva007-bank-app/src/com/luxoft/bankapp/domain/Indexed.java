d42449cc0aeb0a1228b499fc605e77d14f3934a0
boganatar@gmail.com <erb78ioz>
2017-04-03 18:15:56 +0300

    docs

---
+++ docs/JVA-048 Java SE7. Module 4/solutions/m4-jva007-bank-app/src/com/luxoft/bankapp/domain/Indexed.java
@@ -0,0 +1,16 @@
+package com.luxoft.bankapp.domain;
+
+public class Indexed
+{
+    private int id;
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+}
